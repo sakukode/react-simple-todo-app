116868c763b476d093e25b690772c17bf2ddc275
Rizqi Maulana <sakukode@gmail.com>
2021-06-05 08:31:59 +0700

    06 - Mengubah Data

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,24 @@
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, setRefresh }) => {
+
+  const updateTodo = () => {
+    todo.done = !todo.done
+
+    fetch("http://localhost:8000/todos/" + todo.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(todo)
+    }).then(() => {
+      console.log('todo updated.')
+      setRefresh(true)
+    })
+  }
+
   return (
     <li className={`${todo.done ? "checked" : ""}`}>
-      {todo.title} <span className="close">x</span>
+      <div onClick={updateTodo}>{todo.title}</div> 
+      <span className="close">x</span>
     </li>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -28,7 +28,7 @@ const TodoList = ({isRefresh, setRefresh}) => {
   return (
     <ul id="todo-list">
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem todo={todo} key={todo.id} setRefresh={setRefresh} />
       ))}
     </ul>
   );
